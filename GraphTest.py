47d290bcb7e1e54b51e1fd623330127e7c76c7bd
GKAaron <goukegk@gmail.com>
2017-11-08 23:06:43 -0600

    Add 17 buildings' edges. Modify some buildings' access. Delete some intersections. Unify some road names.

--- GraphTest.py
+++ GraphTest.py
@@ -10,44 +10,87 @@ class BuildingGraph():
                           'Atmospheric Science Bldg': {'address': '103 S. Gregory, Urbana', 'code': '223', 'access': 'S. Matthews Avenue'},
                           'Observatory': {'address': '901 S. Mathews, Urbana', 'code': '190', 'access': 'S. Matthews Avenue'},
                           'Undergrad Library': {'address': '1408 W. Gregory, Urbana', 'code': '522', 'access': 'W. Gregory'},
-                          'Armory': {'address': '505 E. Armory, Champaign', 'code': '532', 'access': 'S. 5th'},
-                          'Ice Arena': {'address': '406 E. Armory, Champaign', 'code': '525', 'access': 'E. Armory'},
-                          'Main Library': {'address': '1408 W. Gregory, Urbana', 'code': '522', 'access': 'E. Armory'},
+                          'Armory': {'address': '505 E. Armory, Champaign', 'code': '532', 'access': 'E. Armory'},
+                          'Ice Arena': {'address': '406 E. Armory, Champaign', 'code': '525', 'access': 'S. 5th Street'},
+                          'Main Library': {'address': '1408 W. Gregory, Urbana', 'code': '522', 'access': 'E. Gregory'},
                           'Lincoln Hall': {'address': '702 S. Wright, Urbana', 'code': '456', 'access': 'S. Wright Street'},
                           'Gregory Hall': {'address': '810 S. Wright, Urbana', 'code': '462','access': 'S. Wright Street'},
                           'Psych Bldg': {'address': '603 E. Daniel, Champaign', 'code': '716', 'access': 'S. 6th Street'},
                           'Library & Information Sci': {'address': '501 E. Daniel, Champaign', 'code': '493', 'access': 'E. Daniel Street'},
-                          'SPEECH AND HEARING SCIENCE': {'address': '901 S. Sixth, Champaign', 'code': '482', 'access': 'S. 6th Street'},
+                          'SPEECH AND HEARING SCIENCE': {'address': '901 S. Sixth, Champaign', 'code': '482', 'access': 'E. Daniel Street'},
                           'Illini Union BookStore': {'address': '807 S. Wright, Champaign', 'code': '323', 'access': 'S. Wright Street'},
                           'Coble Hall': {'address': '801 S. Wright, Champaign', 'code': '322', 'access': 'S. Wright Street'},
                           'Illini Hall': {'address': '721 S. Wright, Champaign', 'code': '374', 'access': 'S Wright Street'},
                           'Arcade Bldg': {'address': '620 E. John, Champaign', 'code': '303', 'access': 'S. Wright Street'},
-                          'School of Nursing': {'address': '905 S. Goodwin, Urbana', 'code': '186', 'access': 'S. Goodwin Avenue'},
-                          'Burrill Hall': {'address': '407 S. Goodwin, Urbana', 'code': '114', 'access': 'S Matthews Avenue'},
+                          'School of Nursing': {'address': '905 S. Goodwin, Urbana', 'code': '186', 'access': 'S. Goodwin'},
+                          'Burrill Hall': {'address': '407 S. Goodwin, Urbana', 'code': '114', 'access': 'S. Goodwin'},
                           'Medical Sciences Building': {'address': '506 S. Mathews, Urbana', 'code': '714', 'access': 'S Matthews Avenue'},
-                          'Chem & Life Sci Lab (CLSL)': {'address': '601 S. Goodwin, Urbana', 'code': '123', 'access': 'S Goodwin Avenue'}
+                          'Chem & Life Sci Lab (CLSL)': {'address': '601 S. Goodwin, Urbana', 'code': '123', 'access': 'S Goodwin'}
 
                           }
 
         # intersection = [street_name-street_name]
-        self.intersections = ['S. Goodwin-W.Gregory', 'S. Goodwin-W. Green Street', 'W. Green Street-S. Matthews Avenue',
+        self.intersections = ['S. Goodwin-W. Gregory', 'S. Goodwin-W. Green Street', 'W. Green Street-S. Matthews Avenue',
                               'E. Green Street-S. Wright Street-W. Green Street', 'E. Green-S. 6th Street',
-                              'S. 6th Street-E. Daniel Street'
-                              'E. Daniel-S. Wright Street', 'S. 5th Street-E. Daniel', 'S 5th Street-E. Armory Avenue',
-                              'S 6th Street-E. Armory Avenue', 'S. Wright Street-E. Armory Avenue',
+                              'S. 6th Street-E. Daniel',
+                              'E. Daniel-S. Wright Street', 'S. 5th Street-E. Daniel',
+                              'S 6th Street-E. Armory', 'S. Wright Street-E. Armory',
                               'S. 6th Street-E. Gregory',
-                              'S. Goodwin Avenue-W. Oregon Street']
+                              'S. Goodwin-W. Oregon Street']
 
         # edge = {building_name:{adjacent_node:{distance, street_name, street_direction}}}
-        self.edges = {'Foreign Language Buildings': {
-            'S.Goodwin-W.Gregory': {'distance': 100, 'name': 'S.Goodwin', 'direction': 'East'},
-            'S.Goodwin-W.Green Street': {'distance': 200, 'name': 'S.Goodwin', 'direction': 'West'}},
-                      'S.Goodwin-W.Gregory': {
-                          'Davenport Hall': {'distance': 400, 'name': 'W.Gregory', 'direction': 'North'}},
-                      'S.Goodwin-W.Green Street': {
-                          'Davenport Hall': {'distance': 200, 'name': 'W.Green', 'direction': 'South'}}
-
-                      }
+        self.edges = {
+            'Foreign Language Buildings': {
+                'Davenport Hall': {'distance': 370, 'name': 'S. Matthews Avenue', 'direction': 'North'},
+                'Observatory': {'distance': 378, 'name': 'S. Matthews Avenue', 'direction': 'South'}},
+            'Davenport Hall':{
+                'Foreign Language Buildings':{'distance': 370, 'name': 'S. Matthews Avenue', 'direction': 'South'},
+                'Medical Sciences Building':{'distance': 430, 'name': 'S. Matthews Avenue', 'direction': 'North'}},
+            'Atmospheric Science Bldg':{
+                'Medical Sciences Building':{'distance': 266, 'name': 'S. Matthews Avenue', 'direction': 'South'},
+                'W. Green Street-S. Matthews Avenue':{'distance': 420, 'name': 'S. Matthews Avenue', 'direction': 'North'}},
+            'Observatory':{
+                'Foreign Language Buildings':{'distance': 378, 'name': 'S. Matthews Avenue', 'direction': 'North'}},
+            'Undergrad Library':{
+                'S. Goodwin-W. Gregory':{'distance': 941, 'name': 'W. Gregory', 'direction': 'East'},
+                'Main Library':{'distance': 490, 'name': 'W. Gregory', 'direction': 'West'}},
+            'Armory':{
+                'S 6th Street-E. Armory':{'distance': 456, 'name': 'E. Armory', 'direction': 'East'},
+                'Ice Arena':{'distance': 154, 'name': 'E. Armory', 'direction': 'North'}},
+            'Ice Arena':{
+                'Armory':{'distance': 154, 'name': 'S. 5th Street', 'direction': 'South'},
+                'S. 5th Street-E. Daniel':{'distance': 775, 'name': 'S. 5th Street', 'direction': 'North'}},
+            'Main Library':{
+                'Undergrad Library':{'distance': 490, 'name': 'E. Gregory', 'direction': 'East'},
+                'S. 6th Street-E. Gregory':{'distance': 360, 'name': 'E. Gregory', 'direction': 'West'}},
+            'Lincoln Hall':{
+                'E. Daniel-S. Wright Street':{'distance': 509, 'name': 'S. Wright Street', 'direction': 'North'},
+                'Gregory Hall':{'distance': 402, 'name': 'S. Wright Street', 'direction': 'South'}},
+            'Gregory Hall':{
+                'Lincoln Hall':{'distance': 402, 'name': 'S. Wright Street', 'direction': 'North'},
+                'S 6th Street-E. Armory':{'distance': 405, 'name': 'S. Wright Street', 'direction': 'West'}},
+            'Psych Bldg':{
+                'S 6th Street-E. Armory':{'distance': 743, 'name': 'S. 6th Street', 'direction': 'South'},
+                'S. 6th Street-E. Daniel':{'distance': 180, 'name': 'S. 6th Street', 'direction': 'North'}},
+            'Library & Information Sci':{
+                'S. 5th Street-E. Daniel':{'distance': 129, 'name': 'E. Daniel', 'direction': 'West'},
+                'SPEECH AND HEARING SCIENCE':{'distance': 250, 'name': 'E. Daniel', 'direction': 'East'}},
+            'SPEECH AND HEARING SCIENCE':{
+                'Library & Information Sci':{'distance': 250, 'name': 'E. Daniel', 'direction': 'West'},
+                'S. 6th Street-E. Daniel':{'distance': 90, 'name': 'E. Daniel', 'direction': 'East'}},
+            'Illini Union BookStore':{
+                'E. Daniel-S. Wright Street':{'distance': 117, 'name': 'S. Wright Street', 'direction': 'South'},
+                'Coble Hall':{'distance': 157, 'name': 'S. Wright Street', 'direction': 'North'}},
+            'Coble Hall':{
+                'Illini Union BookStore':{'distance': 157, 'name': 'S. Wright Street', 'direction': 'South'},
+                'Illini Hall':{'distance': 268, 'name': 'S. Wright Street', 'direction': 'North'}},
+            'Illini Hall':{
+                'Coble Hall':{'distance': 268, 'name': 'S. Wright Street', 'direction': 'South'},
+                'Arcade Bldg':{'distance': 121, 'name': 'S. Wright Street', 'direction': 'North'}},
+            'Arcade Bldg':{
+                'Illini Hall':{'distance': 121, 'name': 'S. Wright Street', 'direction': 'South'},
+                'E. Green Street-S. Wright Street-W. Green Street':{'distance': 192, 'name': 'S. Wright Street', 'direction': 'North'}},
+        }
 
     def add_building_nodes(self, G):
         for name, attributes in self.buildings.items():
